5262c5d179a7d714d6b2f953470dfb69a8382697
nishaarya <nishaaryaahmed@gmail.com>
2020-04-28 16:43:21 +0100

    Moved secret_key and debug from settings.py into env

--- djangoC28/settings.py
+++ djangoC28/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uw#f6%w0ul5s(h1@#!tn(w*8aocq3zj4nyar4_hhka4!pg3@uh'
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

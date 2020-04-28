51c7a056446b3b508970241a8f5ef4df3b796468
nishaarya <nishaaryaahmed@gmail.com>
2020-04-28 14:08:16 +0100

    Created a 'Note' class

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Note(models.Model):
+    # unique identifier
+    id = models.UUIDField(primary_key=True, 
+    default=uuid4, editable=False)
+    # The note object has a title and content
+    # CharField is a built in function
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
